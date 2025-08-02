f9ef6b68d6c2f1079c997ea4e7ce88cfcfb5a8b3
Khushi-2306 <khushishetty2307@gmail.com>
2025-08-02 10:32:07 +0530

    Add files via upload

---
+++ k7.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+app=Flask(__name__)
+
+@app.route('/<int:n>')
+def even(n):
+    primes=""
+    for i in range(2,n+1):
+        for n in range(2,i):
+            if(i%n==0):
+                break
+        else:
+                primes = primes + str(i)+","
+    return primes
+
+if __name__=="__main__":
+  app.run()
